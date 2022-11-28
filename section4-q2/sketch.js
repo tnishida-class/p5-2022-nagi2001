4253109e4bea36fca2c85d85270639feca5bc69f
nagi2001 <karaagekun0809@au.com>
2022-11-28 22:19:27 +0900

    change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,18 +1,55 @@
-// テキスト「配列を使った描画」練習問題：円グラフ
-
 function setup(){
   createCanvas(400, 400);
-  background(240);
+  background(255,0,0);
 
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = int(random(20, 100)); // 60以上100未満のランダムな数を代入
   }
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
-  for(let i = 0; i < scores.length; i++){ total += scores[i]; }
+  for(let i = 0; i < scores.length; i++){
+   total += scores[i];
+    }
+ 
+ let sum=0;
+ let z=0;
+ for(let i=0; i < scores.length; i++){
+ sum +=scores[i];
+ let r=sum/total;
+ let R=scores[i]/total;
+ let l=PI*2*r;
+ let m=PI*2*R;
+ let n=l-m;
+ strokeWeight(1);
+ arc(200,200,300,300,n,PI*r*2);
+ strokeWeight(1);
+ line(200+150*cos(PI*r*2),200+150*sin(PI*r*2),200,200);
+ 
 
-  // BLANK[1]
+//balloon(scores[i]);
+
+  }
+
+
+for(let i=0; i < scores.length; i++){
+  sum +=scores[i];
+
+  balloon(scores[i])
+
+  function balloon(t){
+    let r =sum/total;
+
+ let R=scores[i]/total;
+ let l=PI*2*r;
+ let m=PI*2*R;
+ let n=l-m;
+
+    let dx =200+75*cos(PI*2*sum/total);
+    let dy =200+75*sin(PI*2*sum/total);
+    text(t,dx,dy);
+   }
+}
 }
